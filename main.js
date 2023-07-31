9be0c2bfc2ba659c365c6c1beb1adbe627d10071
Cristian Musat <103022491+cmusatmavrck@users.noreply.github.com>
2023-07-31 10:42:34 +0000

    initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+// Add any JavaScript code you want here
+document.addEventListener("DOMContentLoaded", function () {
+    // This function will be executed once the DOM is loaded.
+    // You can add your JavaScript code here.
+    console.log("DOM IS LOADED");
+});
